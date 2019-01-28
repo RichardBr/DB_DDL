68e0e6ba2c77f0f9f2193a6dc6c8c664dabee0ff
Richard Brown <rb010101@hotmail.com>
2019-01-28 11:12:05 +0000

    Added Media types

---
+++ InitialSeedData/Insert_Media_Types.sql
@@ -0,0 +1,13 @@
+USE [Rightmove]
+GO
+
+INSERT INTO [Property].[PropertyMediaTypes] ([Id],[Name]) VALUES
+(1,'Image'),
+(2,'Floorplan'),
+(3,'Brochure'),
+(4,'Virtual Tour'),
+(5,'Audio Tour'),
+(6,'EPC'),
+(7,'EPC Graph')
+
+GO
